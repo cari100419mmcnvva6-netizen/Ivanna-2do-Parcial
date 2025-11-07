f5ddb8e8a2726783c7233790f92cf392a2877d82
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-07 09:34:31 -0600

    1.suma

---
+++ Proyecto p1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // UwU Programa Zip 1: Suma de tres numeros
+    float a, b, c;
+    printf("Este programa Zip suma tres numeros :3\n");
+    printf("Escribe tres números separados por espacio: ");
+    scanf("%f %f %f", &a, &b, &c);
+    printf("El resultado Zipziano es: %.2f\n", a + b + c);
+}
