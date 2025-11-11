a33d5a67700ae88da3728bed1e503e92b168352c
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-11 13:13:13 -0600

    7 numero y su cuadrado

---
+++ 7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+*// Multiplicar por si mismo pidiendolo//*
+main ( ) {
+float Num  ;
+float resultado ;
+ printf ("Ingresa tu Numero");
+ scanf( "%f", &Num);
+ resultado= Num * Num ;
+ printf("El cuadrado es %f ", resultado);
+
+}
+
