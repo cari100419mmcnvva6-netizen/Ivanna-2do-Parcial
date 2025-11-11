fbf66b8dea87ab922817d181a8f87ca0beb8695b
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-11 13:12:13 -0600

    5 coches

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+*// Marca de autos zipzipziana y su modelo kawai //*
+main ( ){
+char Marca [40];
+char Modelo [20];
+ printf ("Ingresa la Marca");
+  scanf("%s", &Marca) ;
+
+printf ("Ingresa la Modelo");
+  scanf("%s", &Modelo) ;
+
+   printf ("modelo %s", Modelo);
+  printf (" marca %s", Marca);
+
+
+
+
+
+
+
+
+}
