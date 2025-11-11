1000c17026b98e268c6652e16cef7ef8867459e0
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-11 13:12:42 -0600

    6 km a millas

---
+++ 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+*// Se convierte de km a millas utilizando una multiplicacion//*
+main ( ) {
+float km ;
+float millas =0.621371 ;
+float resultado ;
+
+  printf ("Ingresa los kilometros");
+  scanf("%f", &km) ;
+
+ resultado= km * millas ;
+ printf("Tu resultado es %f millas", resultado);
+
+}
+
