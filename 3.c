98eaf8db25acf3c284760b52fa09afa17ea4b79a
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-11 13:10:24 -0600

    calcular la calificación final

---
+++ 3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+*// Suma mas division zipzipziana de calificaciones //*
+main ( ){
+ float Cal1;
+ float Cal2;
+ float Cal3;
+ float Cal4;
+ float Resultado;
+
+ printf("Ingresa la calificacion 1");
+  scanf ("%f", &Cal1);
+
+  printf("Ingresa la calificacion 2");
+  scanf ("%f", &Cal2);
+
+ printf("Ingresa la calificacion 3");
+  scanf ("%f", &Cal3);
+
+  printf("Ingresa la calificacion 4");
+  scanf ("%f", &Cal4);
+
+  Resultado = (Cal1 + Cal2 + Cal3 + Cal4)/ 4 ;
+
+   printf("El promedio es %f",Resultado );
+
+
+
+
+}
