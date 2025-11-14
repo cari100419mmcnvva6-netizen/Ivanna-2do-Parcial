d7a73aa347bfcd0d52c4d65318d04e961d1f66ad
cari100419mmcnvva6-netizen <cari100419mmcnvva6@lake.edu.mx>
2025-11-14 09:38:42 -0600

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//multiplicar 3 numeros//
+main (){
+float num1;
+float num2;
+float num3;
+float resultado;
+
+printf("Ingresa tu primer numero");
+scanf ("%f", &num1);
+printf("Ingresa tu segundo numero");
+scanf ("%f", &num2);
+printf("Ingresa tu tercer numero");
+scanf ("%f", &num3);
+
+resultado= num1*num2*num3;
+
+  printf("Resultado:%f", resultado);
+
+}
